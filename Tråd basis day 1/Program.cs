60433a680bd582d2866c385c25837219a4e284f8
Pandaa <TuePVM@gmail.com>
2024-01-22 09:34:23 +0100

    Add project files.

---
+++ Tråd basis day 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tråd_basis_day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadOne = new Thread(new ThreadStart(printTextOne));
+            Thread threadTwo = new Thread(new ThreadStart(printTextTwo));
+
+
+            threadOne.Start();
+            threadTwo.Start();
+
+            Console.ReadLine();
+        }
+
+        static void printTextOne()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("C#-trådning er nemt!");
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void printTextTwo()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Også med flere tråde …");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
